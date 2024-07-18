673e25a551693635b6194bf5e44b889486202a71
PSCmdAssistant <pscmdassistant@microsoft.com>
2024-07-18 19:14:59 +0000

    Autogen code by build fixer

--- src/Compute/Compute/Generated/Models/PSDisk.cs
+++ src/Compute/Compute/Generated/Models/PSDisk.cs
@@ -1,4 +1,4 @@
- //
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -87,4 +87,4 @@ namespace Microsoft.Azure.Commands.Compute.Automation.Models
         public string SecureVMDiskEncryptionSetId { get; set; }
         public string Shield { get; set; }
     }
-}.
+}
