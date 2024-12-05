2ccad572bf049d7e08a72c5c5baab38ea4488905
PSCmdAssistant <pscmdassistant@microsoft.com>
2024-12-05 23:34:24 +0000

    Autogen code

--- src/Compute/Compute/Manual/PSOperationStatusResponse.cs
+++ src/Compute/Compute/Manual/PSOperationStatusResponse.cs
@@ -7,8 +7,7 @@
 //   http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS, either express or implied.
 //
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -26,6 +25,9 @@ namespace Microsoft.Azure.Commands.Compute.Automation.Models
         public DateTime? EndTime { get; set; }
         public string Status  { get; set; }
         public ApiError Error { get; set; }
+        public bool? ScheduledEventsAdditionalEndpoints { get; set; }
+        public bool? EnableUserRebootScheduledEvents { get; set; }
+        public bool? EnableUserRedeployScheduledEvents { get; set; }
 		
 		public PSOperationStatusResponse()
 		{
