a867e60a192edeaa765d30638125d89c0d6f8c38
PSCmdAssistant <pscmdassistant@microsoft.com>
2025-11-17 15:52:22 +0000

    Test generation and changelog update

--- src/Compute/Compute.Test/ScenarioTests/DiskRPTests.cs
+++ src/Compute/Compute.Test/ScenarioTests/DiskRPTests.cs
@@ -219,5 +219,26 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
         {
             TestRunner.RunTestScript("Test-SupportedSecurityOption");
         }
-    }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void testgennewazdiskconfig()
+        {
+            TestRunner.RunTestScript("TestGen-newazdiskconfig");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void testgennewazdiskupdateconfig()
+        {
+            TestRunner.RunTestScript("TestGen-newazdiskupdateconfig");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void testgennewazdisk()
+        {
+            TestRunner.RunTestScript("TestGen-newazdisk");
+        }
+                }
 }
